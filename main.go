f22fcdb52426c63c2d628d6063c9482c213a5679
Kozzen <timotiuswinzen@gmail.com>
2023-11-19 20:58:00 +0700

    add port heroku

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/Kozzen890/project2-group2-glng-ks-08/databases"
 	routers "github.com/Kozzen890/project2-group2-glng-ks-08/router"
 )
 
 func main() {
-	port := ":8080"
+	port := os.Getenv("PORT")
 	log.Printf("Starting server on port %s\n", port)
 	databases.StartDB()
 	routers.InitRouter().Run(port)
